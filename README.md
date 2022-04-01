de3faeb0702544140d3d535cf5f0610242eac169
Emilio Gonzales <emiliog1184@gmail.com>
2022-04-01 11:39:12 -0700

    WIP on replacing values

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ Expected Output: [0, 1, 2, 3, 4, 5]
 Test: "It should use forEach to index and log a number from the array"
 Code: looper(0);
 Expected Output: 0
+
+Test: "It should index the array and tell if there is a 3 in the input"
+Code: looper(13);
+Expected Output: true
--- js/script.js
+++ js/script.js
@@ -4,16 +4,16 @@ function looper(num) {
   let array = []
   for(let i = 0; i<= num; i++) {
     array.push(i)
+    let strA = array.toString();
+    let stSplit = strA.split(",");
+    console.log(stSplit);
+    if (stSplit.includes("3")) {
+      stSplit.replace("3", "Will you be my neighbor?");
+    // } else if (strA.includes("2")) {
+    //   strA.replace("2", "Boop");
+    // } else if (strA.includes("1")) {
+    //   strA.replace("1", "Beep");
+    }
   }
-  console.log("look at these numbers " + array);
-
-  array.forEach(function (element) {
-    element = array[0];
-    console.log(element);
-
-  })
-    return array
+  return array
 };
-
-
-// ui logic
